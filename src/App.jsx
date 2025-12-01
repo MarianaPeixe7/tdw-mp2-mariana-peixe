4e89e16570886f4dc53b1f85a8d76b77f9daa9e4
MarianaPeixe7 <marianapeixe@ua.pt>
2025-12-01 17:23:04 +0000

    loading + atualizacao rotas na app

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Poems from "./pages/Poems";
+import PoemDetail from "./pages/PoemDetail";
 import About from "./pages/About";
 
-
-export default function App() {
+function App() {
   return (
-    <div className="min-h-screen bg-zinc-950 text-zinc-50">
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/poems" element={<Poems />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+    <div className="min-h-screen bg-gray-900 text-gray-100">
+      <Navbar />
+      <div className="p-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/poems" element={<Poems />} />
+          <Route path="/poems/:title" element={<PoemDetail />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </div>
   );
 }
+
+export default App;
