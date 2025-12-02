c5f19183f29d441f2d469c6a062f5144b5cb2cb5
MarianaPeixe7 <marianapeixe@ua.pt>
2025-12-02 23:04:25 +0000

    comentarios api e store

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,7 @@ import favoritesReducer from './favoritesSlice'
 export default configureStore({
   reducer: {
     [poetryApi.reducerPath]: poetryApi.reducer,
-    favorites: favoritesReducer,
+    favorites: favoritesReducer, //gerir estado favoritos, localmente
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(poetryApi.middleware),
